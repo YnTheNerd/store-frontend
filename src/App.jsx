073c1fd705e5a1d3abd322788775f204ec7ef7d9
nnay29 <fodoptallayann@gmail.com>
2025-09-23 17:54:15 +0100

    Exercises 6b to 6d Create checkout folder Replace <a> elements with <Link> components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router';
 import './App.css'
 
 import HomePage from './pages/HomePage';
-import CheckoutPage from './pages/CheckoutPage';
+import CheckoutPage from './pages/checkout/CheckoutPage';
 import { OrdersPage } from './pages/OrdersPage';
 import { TrackingPage } from './pages/TrackingPage';
 

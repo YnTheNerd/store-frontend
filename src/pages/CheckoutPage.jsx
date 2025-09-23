a2fb62cab6bb2bfdf61dd1b61ec7a243c5cdfa78
nnay29 <fodoptallayann@gmail.com>
2025-09-23 17:27:34 +0100

    split header of CheckoutPage into separate CheckoutHeader

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,5 +1,6 @@
-import './CheckoutPage.css' 
-import './checkout-header.css'
+import './CheckoutPage.css'
+import './CheckoutHeader.jsx'
+import { CheckoutHeader } from './CheckoutHeader.jsx';
 
 
 
@@ -7,25 +8,8 @@ function CheckoutPage() {
     return (
         <>
             <title>YN checkout</title>
-            <div className="checkout-header">
-                <div className="header-content">
-                    <div className="checkout-header-left-section">
-                        <a href="/">
-                            <img className="logo" src="images/logo.png" />
-                            <img className="mobile-logo" src="images/mobile-logo.png" />
-                        </a>
-                    </div>
 
-                    <div className="checkout-header-middle-section">
-                        Checkout (<a className="return-to-home-link"
-                            href="/">3 items</a>)
-                    </div>
-
-                    <div className="checkout-header-right-section">
-                        <img src="images/icons/checkout-lock-icon.png" />
-                    </div>
-                </div>
-            </div>
+            <CheckoutHeader />
 
             <div className="checkout-page">
                 <div className="page-title">Review your order</div>
--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -1,5 +1,5 @@
 import './OrdersPage.css'
-
+import { Link } from 'react-router';
 import { Header } from '../components/Header';
 
 
@@ -56,11 +56,11 @@ export function OrdersPage() {
                             </div>
 
                             <div className="product-actions">
-                                <a href="/tracking">
+                                <Link to="/tracking">
                                     <button className="track-package-button button-secondary">
                                         Track package
                                     </button>
-                                </a>
+                                </Link>
                             </div>
 
                             <div className="product-image-container">
@@ -84,11 +84,11 @@ export function OrdersPage() {
                             </div>
 
                             <div className="product-actions">
-                                <a href="/tracking">
+                                <Link to="/tracking">
                                     <button className="track-package-button button-secondary">
                                         Track package
                                     </button>
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
@@ -135,11 +135,11 @@ export function OrdersPage() {
                             </div>
 
                             <div className="product-actions">
-                                <a href="/tracking">
+                                <Link to="/tracking">
                                     <button className="track-package-button button-secondary">
                                         Track package
                                     </button>
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>

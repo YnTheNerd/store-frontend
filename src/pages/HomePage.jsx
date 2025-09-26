fdf763d130f70b82fca36a258da60bce8780e78a
nnay29 <fodoptallayann@gmail.com>
2025-09-26 15:48:20 +0100

    using axios to fetch the data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 
+import axios from 'axios';
 import { Header } from '../components/Header';
 import { products } from '../../starting-code/ecommerce-project/data/products.js';
 import { ProductCard } from '../components/ProductCard.jsx';
@@ -6,15 +7,12 @@ import './HomePage.css';
 
 
 function HomePage() {
-    fetch('http://localhost:3000/api/products')
-
-        .then((response) => {
-
-            return response.json()
-        }).then((data) => {
-            console.log(data)
+    axios
+        .get('http://localhost:3000/api/products')
+        .then((response)=>{
+            console.log(response.data);
         })
-    
+
 
     return (
         <>
